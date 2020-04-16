8cab5c35d470b83c9690b305f8440a3e4276f241
Steven Zuelke <stevezuelkepsn@gmail.com>
2020-04-16 13:06:06 -0700

    CodingStandards Update
    InputFile apparently doesn't work right

--- InputCreator/InputCreator/InputMain.cs
+++ InputCreator/InputCreator/InputMain.cs
@@ -10,10 +10,13 @@ using System.Threading.Tasks;
 
 namespace InputCreator
 {
+
     class InputMain
     {
+
         public static void Main(string[] args)
         {
+
             FileStream fileStream;
             IList<string> text = new List<string>();
             string finalText = "";
@@ -21,7 +24,6 @@ namespace InputCreator
             byte[] output;
 
             fileStream = File.Create("inputFile.txt");
-            
             //Hardcode example cases
             text.Add("4 4"); 
             text.Add("*...");
@@ -32,70 +34,85 @@ namespace InputCreator
             text.Add("**...");
             text.Add(".....");
             text.Add(".*...");
-
             //Hardcode edge cases
             //Minimum size cases
             text.Add("1 1");
             text.Add(".");
             text.Add("1 1");
             text.Add("*");
-
             //Maximum size cases
             //All bombs
             text.Add("100 100");
             for (int i = 0; i < 100; i++)
             {
+
                 text.Add(item: MaxLine('*'));
+
             }
             //No bombs
             text.Add("100 100");
             for (int i = 0; i < 100; i++)
             {
+
                 text.Add(item: MaxLine('.'));
+
             }
             //Some bombs
             text.Add("100 100");
             for (int i = 0; i < 50; i++)
             {
+
                 text.Add(item: MaxLine('.'));
                 text.Add(item: MaxSome());
+
             }
             foreach (string line in text)
             {
+
                 finalText += line + "\n";
+
             }
 
             //Convert to bytes
             output = encoder.GetBytes(finalText);
             fileStream.Write(output, 0, output.Length);
-
             //Close FileStream
             fileStream.Close();
+
         }//End main
 
         //Method to fill a line of 100 chars (c)
 
         private static string MaxLine(char c)
         {
+
             string line = "";
             for (int i = 0; i < 100; i++)
             {
+
                 line += c;
+
             }
             return line;
+
         }
 
         //Method to fill a line with every other . or *
 
         private static string MaxSome()
         {
+
             string line = "";
             for (int i = 0; i < 25; i++)
             {
+
                 line += ".*..";
+
             }
             return line;
+
         }
 
     }
+
 }
