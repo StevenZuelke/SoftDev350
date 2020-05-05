370a46762778ce2a3083aa4ae6de7dd8774bab5b
Steven Zuelke <stevezuelkepsn@gmail.com>
2020-05-05 13:24:49 -0700

    Added ReadQuestions to DataAccess

--- src/DataAccess.java
+++ src/DataAccess.java
@@ -3,10 +3,13 @@ as well as the connection and reading it
 Author: Steven Zuelke
 */
 
+import QuestionTypes.MultipleChoice;
 import QuestionTypes.Question;
+import QuestionTypes.TrueFalse;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
 
@@ -34,34 +37,59 @@ public class DataAccess {
 
     private void ReadQuestions(){
 
-        ReadMultipleChoice();
-        ReadShortAnswer();
-        ReadTrueFalse();
-
-    }
-
-    private void ReadMultipleChoice(){
-        Connection c = null;
+        Connection connection = null;
         try{
-            Class.forName("org.sqlite.JDBC");
-            c = DriverManager.getConnection("jdbc:sqlite:trivia.db");
 
-            Statement statement = c.createStatement();
-            String sql = "";
-            statement.executeUpdate(sql);
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection("jdbc:sqlite:trivia.db");
+            Statement statement = connection.createStatement();
+            //Multiple Choice
+            String sql = "SELECT * FROM MultipleChoice;";
+            ResultSet resultSet = statement.executeQuery(sql);
+            //Add each question in resultSet
+            while(resultSet.next()){
+
+                String title = resultSet.getString("Title");
+                String a = "A: " + resultSet.getString("A");
+                String b = "B: " + resultSet.getString("B");
+                String c = "C: " + resultSet.getString("C");
+                String d = "D: " + resultSet.getString("D");
+                String correct = resultSet.getString("Correct");
+                Questions.add(new MultipleChoice(title, a, b, c, d, correct));
+
+            }
+
+            //True False
+            sql = "SELECT * FROM TrueFalse;";
+            resultSet = statement.executeQuery(sql);
+            //Add each question in resultSet
+            while(resultSet.next()){
+
+                String title = resultSet.getString("Title");
+                String correct = resultSet.getString("Correct");
+                Questions.add(new TrueFalse(title, correct));
+
+            }
+
+            //Short Answer
+            sql = "SELECT * FROM ShortAnswer;";
+            resultSet = statement.executeQuery(sql);
+            //Add each question in resultSet
+            while(resultSet.next()){
+
+                String title = resultSet.getString("Title");
+                String correct = resultSet.getString("Correct");
+                Questions.add(new TrueFalse(title, correct));
+
+            }
+
             statement.close();
 
         } catch (Exception e) {
+
             e.printStackTrace();
+
         }
-        System.out.println("Success");
-    }
-
-    private void ReadShortAnswer(){
-
-    }
-
-    private void ReadTrueFalse(){
 
     }
 
